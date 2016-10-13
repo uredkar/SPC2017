a54baaf989d063b7fc0abb8a007406085847fd3f
unmes <unmes@DESKTOP-383G74D>
2016-10-12 23:15:16 -0500

    y axis showing margin issue

--- src/app/d3jsmodule/d3-demos/simplelinegraph/simple-line-graph.ts
+++ src/app/d3jsmodule/d3-demos/simplelinegraph/simple-line-graph.ts
@@ -8,7 +8,7 @@
 
 import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChange } from '@angular/core';
 
-import { D3Service, D3, ScaleTime, D3DragEvent, D3ZoomEvent, Selection, Line } from 'd3-ng2-service';
+import { D3Service, D3,  ScaleTime, D3DragEvent, D3ZoomEvent, Selection, Line } from 'd3-ng2-service';
 
 import { D3DataService } from '../../d3js.data.service';
 
@@ -22,17 +22,14 @@ export interface LineDatum {
 
 @Component({
     selector: 'app-simple-line-graph',
-    styles: ['.line {  fill: none; stroke: steelblue; stroke-width: 2px;}'],
-    template: '<svg style="stroke-width: 0px; background-color: blue;"> </svg>',
+    template: '<svg width="1060" height="800"></svg>',
     providers: [ D3DataService]
 
 })
 export class SimpleLineGraph implements OnInit,  OnDestroy {
 
-    @Input() width: number = 400;
-    @Input() height: number = 400;
-    @Input() phylloRadius: number = 7;
-    @Input() pointRadius: number = 2;
+    private width: number = 1060;
+    private height: number = 800;
     private d3dataservice: D3DataService;
     private d3: D3;
     private parentNativeElement: any;
@@ -59,65 +56,54 @@ export class SimpleLineGraph implements OnInit,  OnDestroy {
         let d3G: Selection<SVGGElement, any, null, undefined>;
 
 
-        function zoomed(this: SVGSVGElement) {
-            let e: D3ZoomEvent<SVGSVGElement, any> = d3.event;
-            d3G.attr('transform', e.transform.toString());
-        }
-
+   
         if (this.parentNativeElement !== null) {
 
 
-
+            debugger;
             d3ParentElement = d3.select(this.parentNativeElement);
             // set the dimensions and margins of the graph
-            this.margin = { top: 20, right: 20, bottom: 30, left: 50 },
-                this.width = 960 - this.margin.left - this.margin.right,
-                this.height = 500 - this.margin.top - this.margin.bottom;
+            this.margin = { top: 120, right: 120, bottom: 120, left: 120 };
+            this.width = 1060 - this.margin.left - this.margin.right;
+            this.height = 800 - this.margin.top - this.margin.bottom;
 
-            this.d3Svg = d3ParentElement.select<SVGSVGElement>('svg');
-                //.attr("width", width + margin.left + margin.right)
-                //.attr("height", height + margin.top + margin.bottom)
-                //.append("g");
-                             //   .attr("transform",
-                               /// "translate(" + margin.left + "," + margin.top + ")");
-
-            this.d3Svg.attr('width', this.width);
-            this.d3Svg.attr('height', this.height);
-
-            
             // parse the date / time
             var parseTime = d3.timeParse("%d-%b-%y");
 
-            // set the ranges
-            var x = d3.scaleTime().range([0, this.width]);
-            var y = d3.scaleLinear().range([this.height, 0]);
-
-            // define the line
             this.valueline = d3.line<LineDatum>()
                 .x(function (d) { return x(d.date); })
                 .y(function (d) { return y(d.close); });
 
-            // append the svg obgect to the body of the page
-            // appends a 'group' element to 'svg'
-            // moves the 'group' element to the top left margin
-            //var svg = d3.select("body").append("svg")
-            
-            
+
+            this.d3Svg = d3ParentElement.select<SVGSVGElement>('svg')
+                
+            this.d3Svg.attr('width', this.width + this.margin.left + this.margin.right);
+            this.d3Svg.attr('height', this.height + this.margin.top + this.margin.bottom);
+
+            this.d3Svg.append("g")
+                             .attr("transform",
+                              "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+                        
             // Get the data
             var data1 = this.d3dataservice.getClosingPrice();
             var data = data1.map((d) => {
                 return { date: parseTime(d.date), close:  +d.close } ;
             });
+
             // format the data
             var dtdata = data1.map((d) => {
                 return  parseTime(d.date);
             });
 
             var xdomain = d3.extent<Date>(dtdata);
-            // Scale the range of the data
+
+            // set the ranges
+            var x = d3.scaleTime().range([0, this.width]);
+            var y = d3.scaleLinear().range([this.height, 0]);
             x.domain(xdomain);
-            y.domain([0, d3.max(data,  (d) => { return d.close; })]);
-            debugger;
+            y.domain([0, d3.max(data, (d) => { return d.close; })]);
+
             // Add the valueline path.
             this.d3Svg.append("path")
                 .data([data])
@@ -125,12 +111,12 @@ export class SimpleLineGraph implements OnInit,  OnDestroy {
                 .attr("d", this.valueline);
 
             // Add the X Axis
-            this.d3Svg .append("g")
+            this.d3Svg.append("g")
                 .attr("transform", "translate(0," + this.height + ")")
                 .call(d3.axisBottom(x));
 
             // Add the Y Axis
-            this.d3Svg .append("g")
+            this.d3Svg.append("g")
                 .call(d3.axisLeft(y));
 
             
--- src/app/d3jsmodule/d3js.data.service.ts
+++ src/app/d3jsmodule/d3js.data.service.ts
@@ -7,6 +7,7 @@ export class D3DataService {
     getClosingPrice() {
         let prices = [
         { date: '1-May-12', close: 58.13 },
+        { date: '30-Apr-12', close: 53.98},
         { date: '27-Apr-12', close: 67.00},
         { date: '26-Apr-12', close: 89.70},
         { date: '25-Apr-12', close: 99.00},
