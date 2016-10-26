4bfcd9f58933ff31095fb5f124f5d20b4363b7e4
unmes <unmes@DESKTOP-383G74D>
2016-10-26 15:16:00 -0500

    fixing the min height of dynamic forms

--- src/app/menu/sidebar.menu.component.ts
+++ src/app/menu/sidebar.menu.component.ts
@@ -10,6 +10,22 @@ import { Component } from '@angular/core';
 export class SidebarComponent {
     isActive = false;
     showMenu: string = '';
+
+    foods: any[] = [
+        { name: 'Dashboard', route: 'sidemenu1/home' },
+        { name: 'D3JS', route: 'sidemenu1/d31' },
+        { name: 'D3JS', route: 'sidemenu1/d32' },
+        { name: 'D3JS', route: 'sidemenu1/d33' },
+        { name: 'D3JS', route: 'sidemenu1/d34' },
+        { name: 'D3JS', route: 'sidemenu1/d35' },
+        { name: 'threshold', route: 'sidemenu1/threshold' },
+        { name: 'dynamicform', route: 'sidemenu1/dynamicform' },
+        { name: 'grid', route: 'sidemenu1/grid' },
+        { name: 'cross', route: 'sidemenu1/cross' }
+        
+
+    ];
+
     eventCalled() {
         this.isActive = !this.isActive;
     }
